284b69c362738c814ca70fd756c44827a5c1eafe
MohamedAlaa212 <mohamed21alaa@gmail.com>
2023-08-13 18:08:29 -0700

    the for numbers loop

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 1; x < 10; x++)
+	printf("%d\n", x);
+	return (0);
+}
